9f60205b854b912ad746a70606606cc2abba9006
mer <iwangyang@qq.com>
2017-09-14 10:25:52 +0800

    这么厉害吗兄弟

--- C.cpp
+++ C.cpp
@@ -1,11 +1,11 @@
-// c.cpp: 定义控制台应用程序的入口点。
-//
-
+﻿// c.cpp: 定义控制台应用程序的入口点。
+// 编码文件
 #include "stdafx.h"
-
+#include <locale.h>
 
 int main() {
-	printf("Hello World\n");
+	char a = 'a';
+	printf("我是UTF-8文件 中文 不乱码 %c\n", a);
 	getchar();
     return 0;
 }
